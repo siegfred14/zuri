125cd447bcbdcb0526fe8e92fc7fcfd4ac99575c
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-28 20:54:58 +0100

    log in details created

--- 16_ProjectMockATM.py
+++ 16_ProjectMockATM.py
@@ -1,2 +1,11 @@
 username = input('What is your username? \n')
 print(username)
+
+allowedUsers = ['Siegfred', 'Kachi', 'Oluchi', 'Ojochide']
+allowedPassword = "Password"
+
+if(username in allowedUsers):
+    password = input("Your Password? \n")
+
+    if(password == allowedPassword):
+        print('Welcome %s' % username);
