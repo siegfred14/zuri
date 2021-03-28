cbeef23226850d694d511424acc88a1e8fcb7abd
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-28 17:31:14 +0100

    codingEnvironment completed

--- codingEnvironment.py
+++ codingEnvironment.py
@@ -3,9 +3,12 @@ girl = 86
 man = 40
 woman = 50
 
+club = "new club house"
+
 print(boy)
 print(girl)
 print(man)
 print(boy + man)
 print(woman + boy)
-print(man + w) """ Using an IDE like VScode allows you to trace the location of your error at ease by  """
+print(man + woman)
+# Using an IDE like VScode allows you to trace the location of your error at ease
