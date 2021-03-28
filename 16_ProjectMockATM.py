f221dadaa8df8ede6ba8e09764838d1697ecc6ae
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-28 21:29:17 +0100

    password modified as list

--- 16_ProjectMockATM.py
+++ 16_ProjectMockATM.py
@@ -1,8 +1,28 @@
+# username = input('What is your username? \n')
+# print(username)
+#
+# allowedUsers = ['Siegfred', 'Kachi', 'Oluchi', 'Ojochide']
+# allowedPassword = "Password"
+#
+# if(username in allowedUsers):
+#     password = input("Your Password? \n")
+#
+#     if(password == allowedPassword):
+#         print('Welcome %s' % username);
+#     else:
+#         print('Password incorrect, Please try again')
+# else:
+#     print('Name not found. Please Try Again')
+
+    # create form add username and password to list
+
+#     modifying the program to check username and password from list
+
 username = input('What is your username? \n')
 print(username)
 
 allowedUsers = ['Siegfred', 'Kachi', 'Oluchi', 'Ojochide']
-allowedPassword = "Password"
+allowedPassword = ['passSieg', 'passKach', 'passOluchi', 'passChide']
 
 if(username in allowedUsers):
     password = input("Your Password? \n")
@@ -13,6 +33,3 @@ if(username in allowedUsers):
         print('Password incorrect, Please try again')
 else:
     print('Name not found. Please Try Again')
-
-    # create form add username and password to list
-
