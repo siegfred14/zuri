5351599c24d9877b3d6dfb988baa5174bbd8de37
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-31 21:39:58 +0100

    string samples added

--- 08_Data_Types.py
+++ 08_Data_Types.py
@@ -1,3 +1,8 @@
 # String
 # 'string' "word" Strings are always represented in quotes. either single or double
 
+string1 = "string"
+string2 = "4.5"
+string3 = "0"
+string4 = "true"
+string5 = "6" # Once it's in-between quotes, it's a string
