7982904513cc9fa714de729f0bd9c0123dd61b54
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-01 19:37:14 +0100

    comments for explanation added

--- 17_StringOperations_List_Operations_And_Tuples.py
+++ 17_StringOperations_List_Operations_And_Tuples.py
@@ -1,9 +1,12 @@
 # Converting Number Bases
-# from binary to base 10, 0b then the binary value
+# from binary to base 10, 0b or 0B then the binary value
+# (zero + b or B)
 print(0b1110) # Answer is 14
 
 print(0B11) # Answer is 3
 
-# from Octal to Decimal, 0o then binary value
+# from Octal to Decimal, 0o or 0O then binary value
+# (zero + o or O)
+
 print(0o777) # Answer is 511 in decimal
 
