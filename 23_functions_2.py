494ac667b428245a45ef06e8f413cc21782e094b
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-05 13:24:03 +0100

    passing multiple parameters in a function

--- 23_functions_2.py
+++ 23_functions_2.py
@@ -9,3 +9,11 @@ def nameOfFunction(count):
     print(f"This is a function {count}")
 
 nameOfFunction(4)
+
+# Passing multiple parameters
+def nameOfFunction(name, count):
+    print("%s called a function with count %d" %(name, count))
+
+nameOfFunction('Sam', 4)
+nameOfFunction('Jason', 6)
+nameOfFunction('Rachael', 8)
