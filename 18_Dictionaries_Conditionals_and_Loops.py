efaaf3ef9da2dc51f9c61a1c879ebe6fbb4b3e20
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-08 22:11:14 +0100

    Break Statements for Loops

--- 18_Dictionaries_Conditionals_and_Loops.py
+++ 18_Dictionaries_Conditionals_and_Loops.py
@@ -142,3 +142,9 @@ names = ['siegfred', 'toonie', 'tola', 'kuza']
 for char in 'mosquitoes':
     print(char)
 
+# break statement - used to break a loop
+for char in 'mosquitoes':
+    if char == 'u':
+        break
+    print(char) # this stops iterating immediately before u
+
