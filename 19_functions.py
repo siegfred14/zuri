366480e253cd68770cd511ea2fc554fce944596c
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-12 19:30:07 +0100

    function with parameters

--- 19_functions.py
+++ 19_functions.py
@@ -7,3 +7,8 @@ def simple_function():
 # to call the function
 simple_function()
 
+# parameters are added to the function, then called as arguments
+
+def simple_function(name):
+    print(f"Hello {name}")
+
