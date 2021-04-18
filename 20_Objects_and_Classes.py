dc073e8d70fa6b6f3963d7fb734f959626af5409
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-18 15:33:37 +0100

    class definitions

--- 20_Objects_and_Classes.py
+++ 20_Objects_and_Classes.py
@@ -2,3 +2,8 @@
 # Classes provide means by which we bundle related data functionalities together
 # Transmission of real life scenario into programming
 
+# class - car
+# objects - audi, nissan, volvo
+# properties - colour, transmission type, max speed
+# methods - start - stop, accelerate, change transmission
+
