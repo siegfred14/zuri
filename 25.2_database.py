08e5a77a6c00eadb8264dbb424fea696ede83b0b
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-20 19:22:52 +0100

    crud operation functions listed

---
+++ 25.2_database.py
@@ -0,0 +1,25 @@
+# Create records
+# read record
+# update records
+# delete record
+# Find user
+
+
+def create(user_details):
+    print("create a new user record")
+
+
+def read(user_account_number):
+    print("read user record")
+
+
+def update(user_account_number):
+    print("update user record")
+
+
+def delete(user_acoount_number):
+    print("delete_user record")
+
+
+def find(user_account_number):
+    print('find user')
