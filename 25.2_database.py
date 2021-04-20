5e69935c7342ec4369e758212ebeebfeb3b9213b
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-20 22:40:58 +0100

    create function created

--- 25.2_database.py
+++ 25.2_database.py
@@ -5,11 +5,31 @@
 # Find user
 
 
-def create(user_details):
-    print("create a new user record")
+def create(account_number, user_details):
+    completion_state = False
+
+    try:
+        f = open("data/user_record/" + str(account_number) + ".txt", "x")
+
+    except FileExistsError:
+        print('user already exists')
+        # delete the already created file and print out error, then return false
+        return completion_state
+
+    else:
+        f.write(str(user_details))
+        completion_state = True
+
+    finally:
+        f.close()
+
+    return completion_state
+
     # create a file
     # name of file would be account_number.txt
     # add the user details to the file
+    # return true
+    # if saving to file fails, delete created file
 
 
 def read(user_account_number):
@@ -37,4 +57,7 @@ def delete(user_acoount_number):
 def find(user_account_number):
     print('find user')
     # find user record in the data folder
+
+
+create(3440288593, ['Siegfred', 'Samson', 'siegfred@zuri.com', 230])
 
