62837e9f2d5547391b10a2a2670bb1d8929cfab7
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-20 19:52:35 +0100

    25_3_validation module renamed

---
+++ validation.py
@@ -0,0 +1,25 @@
+# user validation 1
+def account_number_validation(account_number):
+    # check if account number is not empty
+    if account_number:
+
+        # check if account number is 10 digits
+        if len(str(account_number)) == 10:
+
+            # ensure account number is an integer by trying to change it to an integer
+            try:
+                int(account_number)
+                return True
+            except ValueError:
+                print('Invalid Account Number, Account Number Should be an Integer')
+                return False
+            except TypeError:
+                print('Invalid Account Type')
+                return False
+
+        else:
+            print('Account number cannot be less or more than 10 digits')
+            return False
+    else:
+        print("Account number is a required field")
+        return False
