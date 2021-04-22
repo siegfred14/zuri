9fe019935209117ac141c3aff502b568e3c7032e
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-22 16:44:56 +0100

    Definitions of terms

--- 20_Objects_and_Classes.py
+++ 20_Objects_and_Classes.py
@@ -2,3 +2,12 @@
 # Classes provide means by which we bundle related data functionalities together
 # Transmission of real life scenario into programming
 
+# class - car
+# objects - audi, nissan, volvo
+# properties - colour, transmission type, max speed
+# methods - start - stop, accelerate, change transmission
+
+# Attributes - properties affiliated with a given class
+# Methods - functions affiliated with some class
+# Instance - object created through calling of that class
+
