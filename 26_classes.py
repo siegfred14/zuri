ec469dfd9962c3bd6866f7acd01197eb5a0496d5
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-30 18:46:51 +0100

    transfef logic updated

--- 26_classes.py
+++ 26_classes.py
@@ -21,8 +21,13 @@ class Budget:
         if self.check_balance(amount) == True:
             self.amount -= amount
             category.amount += amount
+            return f"You have transferred {amount} to {category.category}"
+        else:
+            return "You do not have enough balance to perform this transaction"
+        pass
 
 
 category = Budget("clothing")
+
 category_1 = Budget("Food")
 category_2 = Budget("Entertainment")
