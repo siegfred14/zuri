ce3439590731ca7e233df74818b83b6775b8877d
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-30 18:19:53 +0100

    Budget class with methods added

---
+++ 26_classes.py
@@ -0,0 +1,18 @@
+class Budget:
+
+    def __index__(self, category):
+        self.category - category
+        self.category - 1000
+
+    # methods
+    def deposit(self):
+        return "this is a deposit method"
+
+    def check_balance(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def transfer(self):
+        pass
